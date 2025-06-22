852cd2281d212b56bac6f81a57fb2d9eb98eafca
huyhappy <nguyentanhuy.work@gnail.com>
2025-06-22 19:28:58 +0700

    ++up code

--- hosts/huyhappy/home.nix
+++ hosts/huyhappy/home.nix
@@ -1,69 +1,29 @@
-{ config, pkgs, ... }:
-
-{
+let
+  configDirs = [
+    "hypr"
+    "waybar"
+    "rofi"
+    "kitty"
+    "Thunar"
+    "pulse"
+    "swaync"
+    "fastfetch"
+    "wallust"
+    "wlogout"
+    "xfce4"
+    "swappy"
+    "Kvantum"
+  ];
+in {
   home.username = "huyhappy";
   home.homeDirectory = "/home/huyhappy";
-  home.stateVersion = "25.05"; # Thay bằng phiên bản phù hợp
+  home.stateVersion = "25.05";
 
-  # Sao chép các thư mục cấu hình vào ~/.config/
-  home.file = {
-    ".config/hypr" = {
+  home.file = builtins.listToAttrs (map (name: {
+    name = ".config/${name}";
+    value = {
+      source = ../../dotfiles/${name};
       recursive = true;
-      source = ../../dotfiles/hypr;
     };
-    ".config/fastfetch" = {
-      recursive = true;
-      source = ../../dotfiles/fastfetch;
-    };
-    ".config/kitty" = {
-      recursive = true;
-      source = ../../dotfiles/kitty;
-    };
-    ".config/pulse" = {
-      recursive = true;
-      source = ../../dotfiles/pulse;
-    };
-    ".config/rofi" = {
-      recursive = true;
-      source = ../../dotfiles/rofi;
-    };
-    ".config/swappy" = {
-      recursive = true;
-      source = ../../dotfiles/swappy;
-    };
-    ".config/swaync" = {
-      recursive = true;
-      source = ../../dotfiles/swaync;
-    };
-    ".config/Thunar" = {
-      recursive = true;
-      source = ../../dotfiles/Thunar;
-    };
-    ".config/wallust" = {
-      recursive = true;
-      source = ../../dotfiles/wallust;
-    };
-    ".config/waybar" = {
-      recursive = true;
-      source = ../../dotfiles/waybar;
-    };
-    ".config/wlogout" = {
-      recursive = true;
-      source = ../../dotfiles/wlogout;
-    };
-    ".config/xfce4" = {
-      recursive = true;
-      source = ../../dotfiles/xfce4;
-    };
-  };
-
-  # Cài đặt các gói cần thiết
-  home.packages = with pkgs; [
-    pulseaudio # Hoặc pipewire nếu dùng PipeWire
-    swaynotificationcenter
-    xfce.thunar
-    wallust
-    wlogout
-    xfce.xfce4-settings
-  ];
+  }) configDirs);
 }
